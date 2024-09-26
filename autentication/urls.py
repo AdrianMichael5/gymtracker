7f52cdb848dddee2f0c11b239d13dbb59e2ad49e
AdrianMichael5 <adrian_michael_araujo@hotmail.com>
2024-09-25 23:35:29 -0300

    Urls for singup and login

--- autentication/urls.py
+++ autentication/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from autentication import views
 
 urlpatterns = [
-    path('',views.Home,name="Home")
-
-
+    path('',views.Home,name="Home"),
+    path('signup', views.signup,name="signup"),
+    path('login', views.handlelogin,name="handlelogin"),
 ]
