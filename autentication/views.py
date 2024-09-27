bb1089648fee8709b45bdce7c86b7ab1f7936f98
AdrianMichael5 <adrian_michael_araujo@hotmail.com>
2024-09-27 11:05:37 -0300

    add import contact to views

--- autentication/views.py
+++ autentication/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
+from autentication.models import Contact
 
 def Home(request):
     return render(request,"index.html")
@@ -74,3 +75,16 @@ def handleLogout(request):
     logout(request)
     messages.sucess(request,"Logout Success")
     return redirect('/login')
+
+def contact(request):
+    if request.method=="POST":
+        name=request.POST.get('fullname')
+        email=request.POST.get('email')
+        number=request.POST.get('num')
+        desc=request.POST.get('desc')
+        myquery=Contact(name=name,email=email,phonenumber=number,description=desc)
+        myquery.save()
+        messages.info(request,"Thanks for Contactiong us we will get back you soon")
+        return redirect('/contact')
+
+    return render(request,"contact.html")
