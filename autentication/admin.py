d3484e2b59048940d8434187b1e566f43002cf3f
AdrianMichael5 <adrian_michael_araujo@hotmail.com>
2024-09-29 12:27:58 -0300

    add admin

--- autentication/admin.py
+++ autentication/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from autentication.models import Contact,MembershipPlan,Enrollment,Trainer
-# Register your models here.
 
+from autentication.models import Contact,MembershipPlan,Enrollment,Trainer,Attendance
+
+
+# Register your models here.
 admin.site.register(Contact)
 admin.site.register(MembershipPlan)
 admin.site.register(Enrollment)
 admin.site.register(Trainer)
+admin.site.register(Attendance)
