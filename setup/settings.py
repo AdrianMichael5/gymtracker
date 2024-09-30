daec635ef1db7c406cab1330f5c34883f10d9878
AdrianMichael5 <adrian_michael_araujo@hotmail.com>
2024-09-30 16:51:40 -0300

    settings att

--- setup/settings.py
+++ setup/settings.py
@@ -154,11 +154,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-import os
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+# STATIC_URL = "static/"
+STATIC_URL = os.environ.get('DJANGO_STATIC_URL', "/static/")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
