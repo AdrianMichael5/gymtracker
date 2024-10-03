9640ae1f08eb2a1c70dcfd2053659890417c59cc
AdrianMichael5 <adrian_michael_araujo@hotmail.com>
2024-10-03 16:46:04 -0300

    teste

--- setup/settings.py
+++ setup/settings.py
@@ -140,7 +140,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = os.environ.get('DJANGO_STATIC_URL', "/static/")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_collected')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
